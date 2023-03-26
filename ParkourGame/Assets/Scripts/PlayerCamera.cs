d54e3770b1dfa6e1a2d2472117c6e70b3bdbdc58
hoxyshmoo <52104528+hoxyshmoo@users.noreply.github.com>
2023-03-26 16:58:22 +0100

    Revamped Movement

--- ParkourGame/Assets/Scripts/PlayerCamera.cs
+++ ParkourGame/Assets/Scripts/PlayerCamera.cs
@@ -1,36 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class PlayerCamera : MonoBehaviour
 {
-
-    public float sensX, sensY;
+    public float sensX;
+    public float sensY;
 
     public Transform orientation;
+    public Transform camHolder;
 
-    float xRotation, yRotation;
+    float xRotation;
+    float yRotation;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        Cursor.lockState=CursorLockMode.Locked;
-        Cursor.visible=false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        //get mouse input
-        float mouseX=Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
-        float mouseY=Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+        // get mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
-        yRotation+=mouseX;
-        xRotation-=mouseY;
-        xRotation=Mathf.Clamp(xRotation,-90f,90f);
+        yRotation += mouseX;
 
-        //rotate cam and orientation
-        transform.rotation=Quaternion.Euler(xRotation,yRotation,0);
-        orientation.rotation=Quaternion.Euler(0,yRotation,0);
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // rotate cam and orientation
+        camHolder.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+
+    public void DoFov(float endValue)
+    {
+        GetComponent<Camera>().DOFieldOfView(endValue, 0.25f);
+    }
+
+    public void DoTilt(float zTilt)
+    {
+        transform.DOLocalRotate(new Vector3(0, 0, zTilt), 0.25f);
     }
 }
