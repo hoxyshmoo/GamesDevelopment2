bf090d004346bc182362e748e72d22e4d0fdf0ea
Malik-hw <malekalfilali@gmail.com>
2023-04-03 21:07:49 +0100

    PUSH 6

--- ParkourGame/Assets/Scripts/RespawnScript.cs
+++ ParkourGame/Assets/Scripts/RespawnScript.cs
@@ -11,15 +11,47 @@ public class RespawnScript : MonoBehaviour
     [SerializeField] Transform spawnPoint;
     [SerializeField] float spawnValue;
     [SerializeField] float screamValue;
+    [SerializeField] AudioClip screamAudio;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            gameObject.AddComponent<AudioSource>();
+            audioSource = GetComponent<AudioSource>();
+        }
+        audioSource.clip = screamAudio;
+        audioSource.playOnAwake = false;
+    }
 
     // Update is called once per frame
     void Update()
     {
-          if(player.transform.position.y < -screamValue)
+        if (player.transform.position.y < -screamValue)
         {
-            //play audio here 
+            if (screamAudio != null)
+            {
+                audioSource.PlayOneShot(screamAudio);
+                audioSource.pitch = 1.1f;
+                // Adjust the volume of the audio clip
+                audioSource.volume = 0.05f;
+
+                // Add reverb to the audio clip
+                audioSource.spatialBlend = 1.0f;
+                audioSource.reverbZoneMix = 0.5f;
+
+                // Add distortion to the audio clip
+                audioSource.dopplerLevel = 0.0f;
+                audioSource.spread = 360.0f;
+                audioSource.minDistance = 1.0f;
+                audioSource.maxDistance = 50.0f;
+
+            }
         }
-        if(player.transform.position.y < -spawnValue)
+        if (player.transform.position.y < -spawnValue)
         {
             RespawnPoint();
         }
@@ -31,5 +63,4 @@ public class RespawnScript : MonoBehaviour
         gameManager.LifeLoss(1);
         heartsBar.LoseHeart();
     }
-      
 }
